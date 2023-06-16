6b8e96462cbe3b40a5550f4a1bbe87985120d611
Christian Noss <christian.noss@th-koeln.de>
2023-06-16 12:07:12 +0200

    Added eyecandy

--- scripts/recipe-calculator.js
+++ scripts/recipe-calculator.js
@@ -2,25 +2,32 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     const amount = document.querySelector(".js-recipe-calculator-amount");
     const amountBase = document.querySelector("[data-amount-base]").dataset.amountBase;
+    const recipeCalculatorIngredients = document.querySelector(".js-recipe-calculator-ingredients");
 
     const recalculateRecipe = (value) => {
-        const recipeIngredients = document.querySelectorAll(".js-recipe-calculator-ingredients [data-amount]");
+        const recipeIngredients = recipeCalculatorIngredients.querySelectorAll("[data-amount]");
         recipeIngredients.forEach((recipeIngredient) => {
             const ingredientAmount = recipeIngredient.dataset.amount;
             const ingredientAmountUnit = recipeIngredient.dataset.unit;
-
             const newAmount = (ingredientAmount / amountBase) * value;
+
             recipeIngredient.innerHTML = newAmount + " " + ingredientAmountUnit;
+            recipeCalculatorIngredients.classList.toggle("has-been-changed");
         });
 
-        console.log(amountBase)
     };
 
     amount.addEventListener("change", (event) => {
         event.preventDefault();
         const newValue = event.target.value;
         recalculateRecipe(newValue);
+    });
 
+
+    amount.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const newValue = event.target.querySelector("input").value;
+        recalculateRecipe(newValue);
     });
 
 });
